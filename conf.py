90cbd4d96f768bca6e4778c83b6a8944904a8410
markhenry229 <markhenry229@protonmail.com>
2025-09-25 00:52:19 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -29,6 +29,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # -- Options for HTML output -------------------------------------------------
 
 html_theme = 'alabaster'
+html_title = "QuickBooks Desktop Download and Install 2025: Step-by-Step Guide to Install and Activate for Windows"
 
 # You can still add html_meta here if you want, but with the new Read the Docs addons
 # custom template is the reliable way to inject meta tags.
